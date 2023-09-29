314b0f62d8004b3b0c01a6f7da201c1cdc0d95d9
yonatanlla <yonatanllanto174@gmail.com>
2023-09-29 15:07:45 -0500

    product y categori relacionados

--- src/controllers/postProduct.js
+++ src/controllers/postProduct.js
@@ -1,11 +1,31 @@
 import Product from "../models/product.js";
-import Category from "../models/category.js"
+import Category from "../models/category.js";
+import mongoose from "mongoose";
 
 export const postProduct = async (req, res) => {
   try {
-    const { name, description, price, sizes, colors, images } = req.body;
+    const { name, description, price, sizes, colors, images, categoryId } =
+      req.body;
 
-    console.log({ name, description, price, sizes, colors, images });
+    console.log({
+      name,
+      description,
+      price,
+      sizes,
+      colors,
+      images,
+      categoryId,
+    });
+
+    if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+      throw new Error("Asigna la categoria");
+    }
+
+    const categoryExist = await Category.findById(categoryId);
+
+    if (!categoryExist) {
+      throw new Error(`La categoria ${categoryId} no exist`);
+    }
 
     const newProduct = new Product({
       name,
@@ -14,24 +34,25 @@ export const postProduct = async (req, res) => {
       sizes,
       colors,
       images,
+      category: categoryExist,
     });
     await newProduct.save();
-    res.status(200).json({message: "Producto registrado correctamente"})
+    res.status(200).json({ message: "creado" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Error al registrar el usuario' });
+    res.status(500).json({ message: error.message });
   }
 };
 
 export const postCategory = async (req, res) => {
   try {
-    const { name } = req.body
+    const { name } = req.body;
     const newCategoria = new Category({
-      name
-    })
-    await newCategoria.save()
-    res.status(200).json({messge: "Category created"})
+      name,
+    });
+    await newCategoria.save();
+    res.status(200).json({ messge: "Category created" });
   } catch (error) {
-    res.status(500).json({error: error.message})
+    res.status(500).json({ error: error.message });
   }
-}
+};

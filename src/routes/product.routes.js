d7fe3dd3beb1a13e25719e5a15957ec872906c99
yonatanlla <yonatanllanto174@gmail.com>
2023-10-02 18:11:40 -0500

    put_cateogori

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { postProduct, postCategory } from "../controllers/product/postProduct.js"
 import { getAllProduct } from "../controllers/product/getProducts.js"
+import { putCategories } from "../controllers/product/putProducts.js"
+
 const productRouter = Router()
 
 productRouter.get("/all", getAllProduct)
 productRouter.post("/create",postProduct )
 productRouter.post("/category", postCategory)
+productRouter.put("/:idCategory", putCategories)
 
 export default productRouter
